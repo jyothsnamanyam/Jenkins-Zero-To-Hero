ad77b80335f6f91a407ab9dd7b4936a3374b4910
Jyothsna <JYOTHMA2@novartis.net>
2024-10-24 22:44:20 +0530

    f1

---
+++ jenkins_jobs/spoke_account_access_check.groovy
@@ -0,0 +1,295 @@
+import jenkins.model.*
+import groovy.util.Node
+import java.text.SimpleDateFormat
+
+pipeline {
+    agent any
+    stages {
+        stage('Setup parameters') {
+            steps {
+                script {
+                    properties([
+                        parameters([
+                            [
+                                $class: 'ChoiceParameter',
+                                choiceType: 'PT_SINGLE_SELECT',
+                                description: 'Novartis Environment type',
+                                filterLength: 1,
+                                filterable: false,
+                                name: 'NVS_ENV_TYPE',
+                                randomName: 'choice-parameter-73552873443119',
+                                script: [
+                                    $class: 'GroovyScript',
+                                    fallbackScript: [
+                                    classpath: [],
+                                    sandbox: false,
+                                    script: 'please select environment type'
+                                    ],
+                                    script: [
+                                    classpath: [],
+                                    sandbox: false,
+                                    script:
+                                    """
+                                        import jenkins.model.Jenkins
+                                        def url1 = Jenkins.instance.getRootUrl()
+                                        if ( url1 == "https://aws-infra-cicd-prd.aws.novartis.net/" ){
+                                            return['PROD']
+                                        } else if ( url1 == "https://aws-infra-cicd-devtst.aws.novartis.net/" ){
+                                            return['TEST']
+                                        }
+                                    """
+                                    ]
+                                ]
+                            ],
+                            [
+                                $class: 'ChoiceParameter',
+                                choiceType: 'PT_SINGLE_SELECT',
+                                description: 'Region',
+                                filterLength: 1,
+                                filterable: false,
+                                name: 'RegionName',
+                                randomName: 'enc-choice-parameter-5663098801935',
+                                script: [
+                                        $class: 'GroovyScript',
+                                        fallbackScript: [
+                                        classpath: [],
+                                        sandbox: false,
+                                        script: ''
+                                        ],
+                                        script: [
+                                        classpath: [],
+                                        sandbox: true,
+                                        script:
+                                        """
+										return ['ap-northeast-1']
+										"""
+                                        ]
+                                    ]
+                            ],
+                            [
+                                $class: 'ChoiceParameter',
+                                choiceType: 'PT_SINGLE_SELECT',
+                                description: 'SPOKE ACCOUNT TYPE, Ex: INTERNET.',
+                                filterLength: 1,
+                                filterable: false,
+                                name: 'ACCOUNT_TYPE',
+                                randomName: 'vpc-choice-parameter-5663098801699',
+                                script: [
+                                    $class: 'GroovyScript',
+                                    fallbackScript: [
+                                    classpath: [],
+                                    sandbox: false,
+                                    script: 'return[\'please select \']'
+                                    ],
+                                    script: [
+                                    classpath: [],
+                                    sandbox: true,
+                                    script: 'return[\'INTRANET\', \'INTERNET\', \'ISOLATED\']'
+                                    ]
+                                ]
+                            ],
+                            [
+                                $class: 'ChoiceParameter',
+                                choiceType: 'PT_SINGLE_SELECT',
+                                description: 'Please select The ROLE or IDY ROLE KEYS. ',
+                                filterLength: 1,
+                                filterable: false,
+                                name: 'SPOKE_ACCESS_TYPE',
+                                randomName: 'role-choice-parameter-5663098801655',
+                                script: [
+                                    $class: 'GroovyScript',
+                                    fallbackScript: [
+                                    classpath: [],
+                                    sandbox: true,
+                                    script: 'return[\'How you wants to access the resources\']'
+                                    ],
+                                    script: [
+                                    classpath: [],
+                                    sandbox: true,
+                                    script: 'return[\'ROLE\', \'IDY_ROLE_KEYS\']'
+                                    ]
+                                ]
+                            ],
+                            [
+                                $class: 'DynamicReferenceParameter',
+                                choiceType: 'ET_FORMATTED_HTML',
+                                omitValueField: true,
+                                name: 'SPOKE_idy_accessKeyId',
+                                randomName: 'access-56313144561786299',
+                                referencedParameters: 'SPOKE_ACCESS_TYPE',
+                                script: [
+                                    $class: 'GroovyScript',
+                                    fallbackScript: [
+                                        classpath: [],
+                                        sandbox: true,
+                                        script:
+                                            'return[\' SPOKE_accessKeyId\']'
+                                    ],
+                                    script: [
+                                        classpath: [],
+                                        sandbox: true,
+                                        script:
+                                        """
+                                            if(SPOKE_ACCESS_TYPE.equals('IDY_ROLE_KEYS')) {
+                                                inputBox = "<input name='value' class='setting-input' type='password'>"
+                                            }
+                                            else {
+                                                inputBox="<input name='value' type='text' value='NA' disabled>"
+                                            }
+                                        """
+                                    ]
+                                ]
+                            ],
+							[
+                                $class: 'DynamicReferenceParameter',
+                                choiceType: 'ET_FORMATTED_HTML',
+                                omitValueField: true,
+                                name: 'SPOKE_idy_secretAccessKey',
+                                randomName: 'secret-56313144561786288',
+                                referencedParameters: 'SPOKE_ACCESS_TYPE',
+                                script: [
+                                    $class: 'GroovyScript',
+                                    fallbackScript: [
+                                        classpath: [],
+                                        sandbox: true,
+                                        script:
+                                            'return[\' SPOKE_secretAccessKey\']'
+                                    ],
+                                    script: [
+                                        classpath: [],
+                                        sandbox: true,
+                                        script:
+                                        """
+                                            if(SPOKE_ACCESS_TYPE.equals('IDY_ROLE_KEYS')) {
+												inputBox = "<input name='value' class='setting-input' type='password'>"
+                                            }
+                                            else {
+											inputBox="<input name='value' type='text' value='NA' disabled>"
+                                            }
+                                        """
+                                    ]
+                                ]
+                            ],
+							[
+                                $class: 'DynamicReferenceParameter',
+                                choiceType: 'ET_FORMATTED_HTML',
+                                omitValueField: true,
+                                name: 'SPOKE_idy_sessionToken',
+                                randomName: 'token-56313144561786277',
+                                referencedParameters: 'SPOKE_ACCESS_TYPE',
+                                script: [
+                                    $class: 'GroovyScript',
+                                    fallbackScript: [
+                                        classpath: [],
+                                        sandbox: true,
+                                        script:
+                                            'return[\' SPOKE_sessionToken\']'
+                                    ],
+                                    script: [
+                                        classpath: [],
+                                        sandbox: true,
+                                        script:
+                                        """
+                                            if(SPOKE_ACCESS_TYPE.equals('IDY_ROLE_KEYS')) {
+												inputBox = "<input name='value' class='setting-input' type='password'>"
+                                            }
+                                            else {
+											inputBox="<input name='value' type='text' value='NA' disabled>"
+                                            }
+                                        """
+                                    ]
+                                ]
+                            ],
+                            [
+                                $class: 'DynamicReferenceParameter',
+                                choiceType: 'ET_FORMATTED_HTML',
+                                omitValueField: true,
+                                name: 'SPOKE_idy_role_name',
+                                randomName: 'token-56313144561786266',
+                                referencedParameters: 'SPOKE_ACCESS_TYPE',
+                                script: [
+                                    $class: 'GroovyScript',
+                                    fallbackScript: [
+                                        classpath: [],
+                                        sandbox: true,
+                                        script:
+                                            'return[\' SPOKE_role name\']'
+                                    ],
+                                    script: [
+                                        classpath: [],
+                                        sandbox: true,
+                                        script:
+                                        """
+                                            if(SPOKE_ACCESS_TYPE.equals('IDY_ROLE_KEYS')) {
+												inputBox = "<input name='value' class='setting-input' type='text' value='', placeholder='RIDY_AWS_AWSGLOBALJIT01'>"
+                                            }
+                                            else {
+											inputBox="<input name='value' type='text' value='NA' disabled>"
+                                            }
+                                        """
+                                    ]
+                                ]
+                            ]
+                        ])
+                    ])
+                }
+            }
+        }
+
+		stage('Check Parameters') {
+             steps {
+                 script {
+                     params.each { param ->
+                           //println " '${param.key.trim()}' -> '${param.value.trim()}' "
+                           if (param.value.isEmpty()){
+                             currentBuild.result = 'ABORTED'
+                             println "Please add missing parameter ${param.key.trim()}"
+                             error( 'All parameters are must be provided')
+                           }
+                     }
+                 }
+             }
+        }
+        stage('Approval') {
+             steps {
+                 script {
+                    timeout(time: 5, unit: 'MINUTES'){
+                        input(id: "Deploy Approval", message: "Do you want to proceed for deployment in AWS REGION: ${params.RegionName} ?", ok: 'Deploy')
+                    }
+                 }
+             }
+        }
+        stage('TGW VPC Acceptance Lambda Creation') {
+            steps {
+                script {
+                    sh '''
+                    python3 scripts/spoke_account_access_check.py --env_type=$NVS_ENV_TYPE --region_name=$RegionName --account_type=$ACCOUNT_TYPE --access_type=$SPOKE_ACCESS_TYPE
+                    '''
+                }
+            }
+        }
+    }
+    post {
+        success {
+            script {
+
+                println "Lambda creation was success."
+                def cause = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')
+                env.userId = cause.userId[0]
+                env.userName = cause.userName[0]
+                def d = new Date()
+                def sdf = new SimpleDateFormat ("yyyy-MM-dd HH:mm:ss")
+                sdf.setTimeZone(TimeZone.getTimeZone("IST"))
+                env.currentBuildTime = sdf.format(d)
+                sh '''
+                echo "Success"
+                '''
+            }
+        }
+        failure {
+            script {
+                println "spoke account check was failed."
+            }
+        }
+    }
+}
---
+++ jenkins_jobs/spoke_attachment_propagation.groovy
@@ -0,0 +1,213 @@
+import jenkins.model.*
+import groovy.util.Node
+import java.text.SimpleDateFormat
+
+pipeline {
+    agent any
+    stages {
+        stage('Setup parameters') {
+            steps {
+                script {
+                    properties([
+                        parameters([
+                            [
+                                $class: 'ChoiceParameter',
+                                choiceType: 'PT_SINGLE_SELECT',
+                                description: 'Novartis Environment type',
+                                filterLength: 1,
+                                filterable: false,
+                                name: 'NVS_ENV_TYPE',
+                                randomName: 'choice-parameter-73552873443119',
+                                script: [
+                                    $class: 'GroovyScript',
+                                    fallbackScript: [
+                                    classpath: [],
+                                    sandbox: false,
+                                    script: 'please select environment type'
+                                    ],
+                                    script: [
+                                    classpath: [],
+                                    sandbox: false,
+                                    script:
+                                    """
+                                        import jenkins.model.Jenkins
+                                        def url1 = Jenkins.instance.getRootUrl()
+                                        if ( url1 == "https://aws-infra-cicd-prd.aws.novartis.net/" ){
+                                            return['PROD']
+                                        } else if ( url1 == "https://aws-infra-cicd-devtst.aws.novartis.net/" ){
+                                            return['TEST']
+                                        }
+                                    """
+                                    ]
+                                ]
+                            ],
+                            [
+                                $class: 'CascadeChoiceParameter',
+                                choiceType: 'PT_SINGLE_SELECT',
+                                description: 'Novartis bastion account type',
+                                filterLength: 1,
+                                filterable: false,
+                                name: 'NVS_ACC_TYPE',
+                                randomName: 'choice-parameter-73552873443120',
+                                referencedParameters: 'NVS_ENV_TYPE',
+                                script: [
+                                        $class: 'GroovyScript',
+                                        fallbackScript: [
+                                        classpath: [],
+                                        sandbox: false,
+                                        script: 'please select Novartis bastion account type'
+                                        ],
+                                        script: [
+                                        classpath: [],
+                                        sandbox: false,
+                                        script:
+                                        """
+                                            if(NVS_ENV_TYPE.equals('TEST')) {
+                                                return ['BSTTST']
+                                            }
+                                            else if(NVS_ENV_TYPE.equals('PROD')) {
+                                                return ['BST']
+                                            }
+                                            else {
+                                                return ['NA']
+                                            }
+                                        """
+                                        ]
+                                    ]
+                            ],
+                            [
+                                $class: 'DynamicReferenceParameter',
+                                choiceType: 'ET_FORMATTED_HIDDEN_HTML',
+                                omitValueField: true,
+                                description: 'Please enter the Novartis Source Account ID. ',
+                                name: 'NVS_ACC_ID',
+                                randomName: 'config-parameter-56313144561786258',
+                                referencedParameters: 'NVS_ACC_TYPE',
+                                script: [
+                                    $class: 'GroovyScript',
+                                    fallbackScript: [
+                                        classpath: [],
+                                        sandbox: true,
+                                        script:
+                                            'return[\'Please enter the Novartis Source Account ID.\']'
+                                    ],
+                                    script: [
+                                        classpath: [],
+                                        sandbox: true,
+                                        script:
+                                        """
+                                            if(NVS_ACC_TYPE.equals('BSTTST')) {
+                                                inputBox = "<input name='value' class='setting-input' type='text' value='636711886667'>"
+                                            }
+                                            else if(NVS_ACC_TYPE.equals('BST')) {
+                                                inputBox = "<input name='value' class='setting-input' type='text' value='366103429990'>"
+                                            }
+                                            else {
+                                                inputBox="<input name='value' type='text' value='NA' disabled>"
+                                            }
+                                        """
+                                    ]
+                                ]
+                            ],
+                            [
+                                $class: 'ChoiceParameter',
+                                choiceType: 'PT_SINGLE_SELECT',
+                                description: 'BST Primary region',
+                                filterLength: 1,
+                                filterable: false,
+                                name: 'RegionName',
+                                randomName: 'enc-choice-parameter-5663098801935',
+                                script: [
+                                        $class: 'GroovyScript',
+                                        fallbackScript: [
+                                        classpath: [],
+                                        sandbox: false,
+                                        script: ''
+                                        ],
+                                        script: [
+                                        classpath: [],
+                                        sandbox: true,
+                                        script:
+                                        """
+										return ['ap-northeast-1']
+										"""
+                                        ]
+                                    ]
+                            ],
+                            string(
+                                description: 'Spoke VPC and TGW attachment request creation Date. Ex: 05-May-2023',
+                                name: 'Attach_Date',
+                                defaultValue: '05-May-2023',
+                                trim: true
+                            ),
+                        ])
+                    ])
+                }
+            }
+        }
+
+		stage('Check Parameters') {
+             steps {
+                 script {
+                     params.each { param ->
+                           println " '${param.key.trim()}' -> '${param.value.trim()}' "
+                           if (param.value.isEmpty()){
+                             currentBuild.result = 'ABORTED'
+                             println "Please add missing parameter ${param.key.trim()}"
+                             error( 'All parameters are must be provided')
+                           }
+                     }
+                 }
+             }
+         }
+        stage('Approval') {
+             steps {
+                 script {
+                    timeout(time: 5, unit: 'MINUTES'){
+                        input(id: "Deploy Approval", message: "Do you want to proceed for deployment in AWS REGION: ${params.RegionName} ?", ok: 'Deploy')
+                    }
+                 }
+             }
+         }
+        stage('Spoke VPC attachment propagation.') {
+            steps {
+                script {
+                    def rccAccountId = "304512965277"
+                    def rccRole = rccAdmRole("${NVS_ENV_TYPE}")
+                    rccAssume = assumeRole(rccAccountId, rccRole)
+                    def targetAccountId = "${NVS_ACC_ID}"
+                    def targetRole = bstAdmRole("${NVS_ENV_TYPE}")
+                    targetAssume = assumeRole(targetAccountId, targetRole)
+                    def bstrole = bstEwtpRole("${NVS_ENV_TYPE}")
+                    bstAssume = assumeRole(targetAccountId, bstrole)
+                    sh '''
+                    python3 scripts/spoke_vpc_attachment_propagation.py --environment=$NVS_ENV_TYPE --region_name=$RegionName --attach_date=$Attach_Date
+                    '''
+                }
+            }
+        }
+    }
+    post {
+        success {
+            script {
+
+                println "Spoke vpc propagation was success."
+                def cause = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')
+                env.userId = cause.userId[0]
+                env.userName = cause.userName[0]
+                def d = new Date()
+                def sdf = new SimpleDateFormat ("yyyy-MM-dd HH:mm:ss")
+                sdf.setTimeZone(TimeZone.getTimeZone("IST"))
+                env.currentBuildTime = sdf.format(d)
+                sh '''
+                echo "Success"
+                '''
+            }
+        }
+        failure {
+            script {
+                println "Spoke vpc propagation was failed."
+            }
+        }
+    }
+}
